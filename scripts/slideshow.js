5d766689f3e96b72c26c9790a69b2749bd7f11c7
Yann Landry <landryy@twitch.tv>
2022-01-27 02:12:00 -0800

    Add progress bar

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -3,19 +3,19 @@
     if (slides.length < 2) {
         return;
     }
+    var progress = document.getElementById("progress-bar");
 
     var preload = function(i) {
         if (slides.item(i).style["background-image"] == "" && "background" in slides.item(i).dataset) {
             slides.item(i).style["background-image"] = "url(\"" + slides.item(i).dataset["background"] + "\")";
-            return true;
         }
-        return false;
     }
 
     var current = slides.length - 1;
     var previous = current - 1;
     var next = 0;
-    var last = Date.now();
+    var cycle = 0;
+    var interval = 9;
 
     // should already be loaded via HTML but just in case
     preload(current);
@@ -26,26 +26,26 @@
             return;
         }
 
-        var now = Date.now();
-        if (now - last > 7000 && preload(next)) {
-            // if there's ~5s left or less until the next switch and the next image is not loaded, wait another 4s
-            return;
-        }
-        if (now - last < 11000) {
-            // if the next slide is loaded but we've run less than 11s ago, wait another 4s
-            return;
-        }
-        // perform the switch and update the timestamp
-        last = now;
+        cycle = (cycle + 1) % interval;
 
-        slides.item(previous).style["z-index"] = "0";
-        slides.item(previous).style["opacity"] = "1";
-        slides.item(current).style["z-index"] = "2";
-        slides.item(next).style["z-index"] = "1";
-        slides.item(current).style["opacity"] = "0";
+        if (cycle == interval - 3) {
+            // preload the next slide 3 cycles before the interval
+            preload(next);
+        }
 
-        previous = current;
-        current = next;
-        next = (next + 1) % slides.length;
-    }, 4000);
+        if (cycle == 0) {
+            // on cycle 0 (first is cycle 1), rotate to the next slide
+            slides.item(previous).style["z-index"] = "0";
+            slides.item(previous).style["opacity"] = "1";
+            slides.item(current).style["z-index"] = "2";
+            slides.item(next).style["z-index"] = "1";
+            slides.item(current).style["opacity"] = "0";
+            previous = current;
+            current = next;
+            next = (next + 1) % slides.length;
+        }
+
+        // adjust progress bar
+        progress.style["width"] = (100 / (interval - 1) * cycle) + "%";
+    }, 1000);
 })();
